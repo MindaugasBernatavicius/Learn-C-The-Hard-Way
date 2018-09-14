503208b172121312759c9a4ef94b0ebc57b3ea4f
mindaugasb <darbas.mindaugas@gmail.com>
2018-09-14 05:52:43 +0000

    Adding more complex make file to ex1-hw

---
+++ ex1-hw/ex1.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char * argv[]){
+  int dist = 100;
+  // this is also a comment
+  printf("You are %d miles away.\n", dist);
+  return 0;
+}
